cca6f415231c3c922c3d1454156b50eace34a6f2
youssefsalehs <youssef.ashraf.saleh@gmail.com>
2026-02-16 21:05:20 +0200

    fix: Update createAlias to handle missing originalUrl and generate random alias if not provided

--- controller/urlController.js
+++ controller/urlController.js
@@ -26,16 +26,19 @@ const getAllUrls = async (req, res, next) => {
 const createAlias = async (req, res, next) => {
   try {
     const { originalUrl, alias } = req.body;
-    if (!originalUrl || !alias) {
-      return next(createHttpError(400, "Missing fields"));
+    if (!originalUrl) {
+      return next(createHttpError(400, "You should enter an url"));
     }
-
-    const exists = await Url.findOne({ alias });
+    let finalAlias = alias;
+    if (!alias || alias.trim() === "") {
+      finalAlias = Math.random().toString(36).substring(2, 8);
+    }
+    const exists = await Url.findOne({ alias: finalAlias });
     if (exists) {
       return next(createHttpError(400, "Alias already exists"));
     }
 
-    await Url.create({ originalUrl, alias });
+    await Url.create({ originalUrl, alias: finalAlias });
 
     res.redirect("/");
   } catch (err) {
